b9f0edf731f49a89d300458b445a8f06665fb8b8
Gustavo Silva <gug.contato@gmail.com>
2024-05-14 18:06:26 -0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import Badge from "./components/badge/Badge.vue";
 import Button from "./components/button/Button.vue";
+import Calendar from "./components/calendar/Calendar.vue";
 import Portal from "./components/portal/Portal.vue"
 import Modal from "./components/modal/Modal.vue";
 
 export {
     Badge,
     Button,
+    Calendar,
     Portal,
-    Modal
+    Modal,
 };
