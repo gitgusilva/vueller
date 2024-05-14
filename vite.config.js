35ab4382a0861ab5e3ee55c728b21c95025f691f
Gustavo Silva <gug.contato@gmail.com>
2024-05-13 23:03:23 -0300

    fix css

--- vite.config.js
+++ vite.config.js
@@ -20,19 +20,8 @@ export default defineConfig({
       output: {
         globals: {
           vue: 'Vue',
-        },
-      },
-      // Ignorar arquivos .scss
-      plugins: [
-        {
-          name: 'ignore-sass',
-          resolveId(id) {
-            if (id.endsWith('.scss')) {
-              return { id: 'ignored' };
-            }
-          }
         }
-      ]
+      }
     },
     // Configuração para minificar usando Terser
     minify: 'terser',
